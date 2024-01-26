b8290718f5f3da78d12247bd2a1d9bbd6dbc55c4
YASIR MALIK <yasirrock6@gmail.com>
2024-01-26 22:03:52 +0500

    changes fonts and images

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,27 @@
-var typed = new Typed('#element', {
-strings: ['software solution', 'with Qonkar', 'digital ideas'],
-      typeSpeed: 60,
-      loop:true, 
-      backDelay:500,
-    });
-    
-  
+var typed = new Typed("#element", {
+  strings: ["software solution", "with Qonkar", "digital ideas"],
+  typeSpeed: 60,
+  loop: true,
+  backDelay: 500,
+});
 
+let portfolioSwiper = new Swiper(".portfolio-container", {
+  grabCursor: true,
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  slidesPerView: 4,
+  loop: true,
+  speed: 5000,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+  },
+});
+document.querySelector(".portfolio-container").addEventListener("click", function () {
+  portfolioSwiper.stopAutoplay();
+  console.log("portfolioSwiper");
+});
+
+document.querySelector(".portfolio-container").addEventListener("mouseleave", function () {
+  portfolioSwiper.startAutoplay();
+});
