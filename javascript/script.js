ecfd590e12b6ccb4d847f631750e23a7b822da3f
YASIR MALIK <yasirrock6@gmail.com>
2024-03-04 19:55:55 +0500

    footer added

--- javascript/script.js
+++ javascript/script.js
@@ -3,13 +3,87 @@ setTimeout(function () {
   loader.style.top = "-100%";
 }, 2500);
 
+const bankAccounts = document.getElementById("bank-accounts");
+
+let isDown = false;
+let startX;
+let startY;
+let scrollLeft;
+let scrollTop;
+
+bankAccounts.addEventListener("mousedown", (e) => {
+  isDown = true;
+  startX = e.pageX - bankAccounts.offsetLeft;
+  startY = e.pageY - bankAccounts.offsetTop;
+  scrollLeft = bankAccounts.scrollLeft;
+  scrollTop = bankAccounts.scrollTop;
+  bankAccounts.style.cursor = "grabbing";
+});
+
+bankAccounts.addEventListener("mouseleave", () => {
+  isDown = false;
+  bankAccounts.style.cursor = "grab";
+});
+
+bankAccounts.addEventListener("mouseup", () => {
+  isDown = false;
+  bankAccounts.style.cursor = "grab";
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - bankAccounts.offsetLeft;
+  const y = e.pageY - bankAccounts.offsetTop;
+  const walkX = (x - startX) * 1;
+  const walkY = (y - startY) * 1;
+  bankAccounts.scrollLeft = scrollLeft - walkX;
+  bankAccounts.scrollTop = scrollTop - walkY;
+});
+
+const scrollLeftButton = document.getElementById("action-button--previous");
+const scrollRightButton = document.getElementById("action-button--next");
+
+scrollLeftButton.addEventListener("click", () => {
+  bankAccounts.scrollBy({
+    top: 0,
+    left: -200,
+    behavior: "smooth",
+  });
+});
+
+scrollRightButton.addEventListener("click", () => {
+  bankAccounts.scrollBy({
+    top: 0,
+    left: 200,
+    behavior: "smooth",
+  });
+});
+
+bankAccounts.addEventListener("scroll", (e) => {
+  const position = bankAccounts.scrollLeft;
+  if (position === 0) {
+    scrollLeftButton.disabled = true;
+  } else {
+    scrollLeftButton.disabled = false;
+  }
+
+  if (Math.round(position) === bankAccounts.scrollWidth - bankAccounts.clientWidth) {
+    scrollRightButton.disabled = true;
+  } else {
+    scrollRightButton.disabled = false;
+  }
+});
+
+// ----------//
+
 let portfolioSwiper = new Swiper(".portfolio-container", {
   grabCursor: true,
   slidesPerView: "auto",
-  spaceBetween: 20,
+  spaceBetween: 10,
   slidesPerView: 4,
   loop: true,
-  speed: 3000,
+  speed: 7000,
   parallax: true,
   autoplayDisableOnInteraction: false,
   autoplay: {
@@ -27,12 +101,16 @@ let portfolioSwiper = new Swiper(".portfolio-container", {
     // when window width is >= 480px
     480: {
       slidesPerView: 2,
-      spaceBetween: 30,
+      spaceBetween: 20,
     },
     // when window width is >= 640px
     640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    840: {
       slidesPerView: 4,
-      spaceBetween: 40,
+      spaceBetween: 20,
     },
   },
 });
