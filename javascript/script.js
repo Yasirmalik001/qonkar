c1d63d55618d118df1f1bb9643635364c2b3f200
YASIR MALIK <yasirrock6@gmail.com>
2024-03-06 00:37:46 +0500

    mediaquieres

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,44 @@ setTimeout(function () {
   loader.style.top = "-100%";
 }, 2500);
 
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.body.classList.contains("homepage")) {
+    const header = document.querySelector(".header-container");
+    var btn = document.querySelector(".circle__up");
+    const checkScroll = () => {
+      switch (true) {
+        case window.scrollY < 100:
+          btn.style.display = "none";
+          break;
+        case window.scrollY >= 400:
+          btn.style.display = "block";
+          break;
+      }
+    };
+
+    checkScroll();
+    window.addEventListener("scroll", checkScroll);
+  }
+});
+
+var btn = document.querySelector(".circle__up");
+btn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+var typed2 = new Typed(".typed", {
+  strings: ["digital <strong>IDEAS</strong>", "digital <strong>SUCCESS</strong>", "UI/UX <strong>DESIGN</strong>"],
+  typeSpeed: 50,
+  backSpeed: 20,
+  cursorChar: "_",
+  shuffle: true,
+  fadeOut: true,
+  loop: true,
+});
+
 const bankAccounts = document.getElementById("bank-accounts");
 
 let isDown = false;
