ddd189f69b5dae5d3474a9e4ae9a2115b6764e71
YASIR MALIK <yasirrock6@gmail.com>
2024-03-16 16:08:40 +0500

    mobile responsive

--- javascript/script.js
+++ javascript/script.js
@@ -113,24 +113,6 @@ bankAccounts.addEventListener("scroll", (e) => {
 });
 
 // ----------//
-// var $header_top = $(".header-top");
-// var $nav = $("nav");
-
-// $header_top.find("a").on("click", function () {
-//   $(this).parent().toggleClass("open-menu");
-// });
-
-// $("#fullpage").fullpage({
-//   sectionsColor: ["#B8AE9C", "#348899", "#F2AE72", "#5C832F", "#B8B89F", "#5C832F", "#B8B89F"],
-//   sectionSelector: ".vertical-scrolling",
-//   slideSelector: ".horizontal-scrolling",
-//   navigation: true,
-//   slidesNavigation: true,
-//   controlArrows: false,
-//   menu: "#menu",
-// });
-
-// ----------//
 
 let portfolioSwiper = new Swiper(".portfolio-container", {
   grabCursor: true,
